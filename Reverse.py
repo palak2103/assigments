bd244483e70dcbd05e9f0091bbcb858940196d21
Palak Gupta <palakgupta120898@gmail.com>
2022-03-30 16:20:37 +0530

    feat:

---
+++ Reverse.py
@@ -0,0 +1,8 @@
+#Write function that reverses a list, preferably in place.
+def reverse():
+    list = [21,3,4,65,78,9,7,65,45,34,324,6,5]
+    list.reverse()
+    i=-1
+    for i in list:
+        print(i,end=" ")
+reverse()
