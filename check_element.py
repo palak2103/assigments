c9335734b0620cbe9b08e0a6076bbb614b40dcdd
Palak Gupta <palakgupta120898@gmail.com>
2022-03-30 18:03:42 +0530

    feat: :white_check_mark:

---
+++ check_element.py
@@ -0,0 +1,9 @@
+#write a function that checks whether an element occurs in a list
+
+
+list = [21,3,4,65,78,9,7,65,45,34,324,6,5]
+a=int(input("enter"))
+if a in list:
+    print("this is in a list",a)
+else:
+    print("this is not in  a list ")
