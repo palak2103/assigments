44535cb531f27cdbdbbbbaedeb7e1c4c6de48468
Palak Gupta <palakgupta120898@gmail.com>
2022-03-30 18:01:21 +0530

    style: :white_check_mark:

---
+++ greet_with_name.py
@@ -0,0 +1,4 @@
+#Write a program that asks the user for their name and greets them with their name.
+user_name=input("enter the name   ")
+greet="GOOD MORNING HAVE A NICE DAY"
+print(greet,user_name)
