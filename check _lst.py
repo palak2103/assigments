e736df6d35ed122b02feaf8364443b8763ae39eb
Palak Gupta <palakgupta120898@gmail.com>
2022-04-03 17:30:26 +0530

    check list

---
+++ check _lst.py
@@ -0,0 +1,17 @@
+# Write a function that checks whether an element occurs in a list.
+
+# lst=[12,15,18,2,88,96,]
+# b=int(input("enter the value "))
+# if b in lst:
+#     print("this is in a lst ",b)
+# else:
+#     print("this is not in a lst")
+def check_list(b):
+    lst=[12,15,18,2,88,96,]
+    # b=int(input("enter the value "))
+    if b in lst:
+        print("this is in a lst ", b)
+    else:
+        print("this is not in a lst ", b)  
+check_list(0)  
+
